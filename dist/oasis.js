80687eb93e8054fe0df70ac71f1f20a4a779b962
Brynhuda <brynhuda@tilde.io>
2013-02-18 14:45:33 -0800

    Add documentation

--- dist/oasis.js
+++ dist/oasis.js
@@ -556,10 +556,92 @@ define("oasis",
       }
     };
 
+    /**
+      This is the entry point that allows the containing environment to create a
+      child sandbox.
+
+      Options:
+
+      * `capabilities`: an array of registered services
+      * `url`: a registered URL to a JavaScript file that will initialize the
+        sandbox in the sandboxed environment
+      * `adapter`: a reference to an adapter that will handle the lifecycle
+        of the sandbox. Right now, there are iframe and web worker adapters.
+
+      @param {Object} options
+    */
     Oasis.createSandbox = function(options) {
       return new OasisSandbox(options);
     };
 
+    /**
+      This is a base class that services and consumers can subclass to easily
+      implement a number of events and requests at once.
+
+      Example:
+
+          var MetadataService = Oasis.Service.extend({
+            initialize: function() {
+              this.send('data', this.sandbox.data);
+            },
+
+            events: {
+              changed: function(data) {
+                this.sandbox.data = data;
+              }
+            },
+
+            requests: {
+              valueForProperty: function(name, promise) {
+                promise.resolve(this.sandbox.data[name]);
+              }
+            }
+          });
+
+      In the above example, the metadata service implements the Service
+      API using `initialize`, `events` and `requests`.
+
+      Both services (implemented in the containing environment) and
+      consumers (implemented in the sandbox) use the same API for
+      registering events and requests.
+
+      In the containing environment, a service is registered in the
+      `createSandbox` method. In the sandbox, a consumer is registered
+      using `Oasis.connect`.
+
+      ### `initialize`
+
+      Oasis calls the `initialize` method once the other side of the
+      connection has initialized the connection.
+
+      This method is useful to pass initial data back to the other side
+      of the connection. You can also set up events or requests manually,
+      but you will usually want to use the `events` and `requests` sections
+      for events and requests.
+
+      ### `events`
+
+      The `events` object is a list of event names and associated callbacks.
+      Oasis will automatically set up listeners for each named event, and
+      trigger the callback with the data provided by the other side of the
+      connection.
+
+      ### `requests`
+
+      The `requests` object is a list of request names and associated
+      callbacks. Oasis will automatically set up listeners for requests
+      made by the other side of the connection, and trigger the callback
+      with the request information as well as a promise that you should
+      use to fulfill the request.
+
+      Once you have the information requested, you should call
+      `promise.resolve` with the response data.
+
+      @constructor
+      @param {OasisPort} port
+      @param {OasisSandbox} sandbox in the containing environment, the
+        OasisSandbox that this service is connected to.
+    */
     Oasis.Service = function(port, sandbox) {
       var service = this, prop, callback;
 
@@ -584,12 +666,41 @@ define("oasis",
     };
 
     Oasis.Service.prototype = {
+      /**
+        This hook is called when the connection is established. When
+        `initialized` is called, it is safe to register listeners and
+        send data to the other side.
+
+        The implementation of Oasis makes it impossible for messages
+        to get dropped on the floor due to timing issues.
+
+        @param {OasisPort} port the port to the other side of the connection
+        @param {String} name the name of the service
+      */
       initialize: function() {},
 
+      /**
+        This method can be used to send events to the other side of the
+        connection.
+
+        @param {String} eventName the name of the event to send to the
+          other side of the connection
+        @param {Structured} data an additional piece of data to include
+          as the data for the event.
+      */
       send: function() {
         return this.port.send.apply(this.port, arguments);
       },
 
+      /**
+        This method can be used to request data from the other side of
+        the connection.
+
+        @param {String} requestName the name of the request to send to
+          the other side of the connection.
+        @return {Promise} a promise that will be resolved by the other
+          side of the connection. Use `.then` to wait for the resolution.
+      */
       request: function() {
         return this.port.request.apply(this.port, arguments);
       }
@@ -651,14 +762,69 @@ define("oasis",
       };
     }
 
+    /**
+      OasisPort is an interface that adapters can use to implement ports.
+      Ports are passed into the `initialize` method of services and consumers,
+      and are available as `this.port` on services and consumers.
+
+      Ports are the low-level API that can be used to communicate with the
+      other side of a connection. In general, you will probably want to use
+      the `events` and `requests` objects inside your service or consumer
+      rather than manually listen for events and requests.
+
+      @constructor
+      @param {OasisPort} port
+    */
     function OasisPort(port) {}
 
     OasisPort.prototype = {
+      /**
+        This allows you to register an event handler for a particular event
+        name.
+
+        @param {String} eventName the name of the event
+        @param {Function} callback the callback to call when the event occurs
+        @param {any?} binding an optional value of `this` inside of the callback
+      */
       on: mustImplement('OasisPort', 'on'),
+
+      /**
+        This allows you to unregister an event handler for an event name
+        and callback. You should not pass in the optional binding.
+
+        @param {String} eventName the name of the event
+        @param {Function} callback a reference to the callback that was
+          passed into `.on`.
+      */
       off: mustImplement('OasisPort', 'off'),
+
+      /**
+        This method sends an event to the other side of the connection.
+
+        @param {String} eventName the name of the event
+        @param {Structured?} data optional data to pass along with the event
+      */
       send: mustImplement('OasisPort', 'send'),
+
+      /**
+        @private
+
+        Adapters should implement this to start receiving messages from the
+        other side of the connection.
+
+        It is up to the adapter to make sure that no messages are dropped if
+        they are sent before `start` is called.
+      */
       start: mustImplement('OasisPort', 'start'),
 
+      /**
+        This method sends a request to the other side of the connection.
+
+        @param {String} requestName the name of the request
+        @return {Promise} a promise that will be resolved with the value
+          provided by the other side of the connection. The fulfillment value
+          must be structured data.
+      */
       request: function(eventName) {
         var promise = new RSVP.Promise();
         var requestId = getRequestId();
@@ -676,6 +842,18 @@ define("oasis",
         return promise;
       },
 
+      /**
+        This method registers a callback to be called when a request is made
+        by the other side of the connection.
+
+        The callback will be called with a `resolver` to a promise, that the
+        callback should resolve with the fulfillment value.
+
+        @param {String} requestName the name of the request
+        @param {Function} callback the callback to be called when a request
+          is made.
+        @param {any?} binding the value of `this` in the callback
+      */
       onRequest: function(eventName, callback, binding) {
         var self = this;
 
@@ -754,6 +932,18 @@ define("oasis",
       }
     });
 
+    /**
+      This registers a sandbox type inside of the containing environment so that
+      it can be referenced by URL in `createSandbox`.
+
+      Options:
+
+      * `capabilities`: An array of service names that will be supplied when calling
+        `createSandbox`
+      * `url`: The URL of the JavaScript file that contains the sandbox code
+
+      @param {Object} options
+    */
     Oasis.register = function(options) {
       assert(options.capabilities, "You are trying to register a package without any capabilities. Please provide a list of requested capabilities, or an empty array ([]).");
 
@@ -775,6 +965,20 @@ define("oasis",
 
     Oasis.consumers = {};
 
+    /**
+      This is the main entry point that allows sandboxes to connect back
+      to their containing environment.
+
+      It should be called once for each service provided by the containing
+      environment that it wants to connect to.
+
+      @param {String} serviceName the name of the service to connect to
+      @param {Function?} callback the callback to trigger once the other
+        side of the connection is available
+      @return {Promise} a promise that will be resolved once the other
+        side of the connection is available. You can use this instead
+        of the callback.
+    */
     Oasis.connect = function(capability, callback) {
       function setupCapability(Consumer, name) {
         return function(port) {
