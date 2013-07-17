b7104707da92deb5b5087a39633bf1b96c0473ec
David J. Hamilton <davidjh@hjdivad.com>
2013-07-17 12:30:32 -0700

    Fix #24.

--- test/fixtures/connect_failed_consumer.js
+++ test/fixtures/connect_failed_consumer.js
@@ -2,6 +2,13 @@ Oasis.connect('assertions').then(function (port) {
   Oasis.connect({
     consumers: {
       unprovidedCapability: Oasis.Consumer.extend({
+        events: {
+          doAThing: function () {}
+        },
+        requests: {
+          getSomeStuff: function () {}
+        },
+
         error: function () {
           port.send('consumerErrorInvoked');
         }
