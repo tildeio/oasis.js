999d32d5e977ce4235ae1e8b14f23869df45ab79
David J. Hamilton <davidjh@hjdivad.com>
2013-09-30 19:40:28 -0700

    wip

---
+++ tasks/release-branch.js
@@ -0,0 +1,11 @@
+module.exports = function (grunt) {
+  // TODO: clone repo in tmp
+  // checkout branch
+  // copy bower.json & dist/*
+  // make commit
+  // force push
+
+  grunt.registerTask('release-branch', 'push the built files in the workspace to the dist repository, under a branch that matches the current branch name.', function () {
+    
+  });
+};
