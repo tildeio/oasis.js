8864945bc504e8753452447c5164b702a60baf27
Cyril Fluck <github@fluck.fr>
2014-05-21 20:25:37 -0700

    Try to fix the `Class doesn't support automation` issue
    [Fixes #78]

--- test/helpers/async.js
+++ test/helpers/async.js
@@ -1,8 +1,20 @@
+/*global XDomainRequest */
 var xhrs = [];
 
 function xhrDone(url, options, xhr) {
+  var compareXhr = function(xhr1, xhr2) {
+    var result;
+    if( typeof XDomainRequest !== 'undefined' && xhr1 instanceof XDomainRequest ) {
+      result =  (xhr1.contentType === xhr2.contentType) &&
+                (xhr1.responseText === xhr2.responseText);
+    } else {
+      result =  xhr1 === xhr2;
+    }
+    return result;
+  };
+
   for (var i=0; i<xhrs.length; ++i) {
-    if (xhrs[i].xhr === xhr) {
+    if( compareXhr(xhrs[i].xhr, xhr) ) {
       xhrs.splice(i, 1);
       break;
     }
